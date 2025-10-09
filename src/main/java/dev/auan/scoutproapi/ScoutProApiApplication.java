a7859dab0c4257777755b614fe2ed7559f75d29d
AuanJulio <auanjulio13@gmail.com>
2025-10-09 01:04:38 -0300

    first commit

---
+++ src/main/java/dev/auan/scoutproapi/ScoutProApiApplication.java
@@ -0,0 +1,13 @@
+package dev.auan.scoutproapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScoutProApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ScoutProApiApplication.class, args);
+    }
+
+}
